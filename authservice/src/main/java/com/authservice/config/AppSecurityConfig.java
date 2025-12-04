5f717e67f67f98ef31e1d0f80cb0640cdeb2da49
Vishal-1007 <vg03122001@gmail.com>
2025-12-04 19:29:58 +0530

    Application updated.

--- authservice/src/main/java/com/authservice/config/AppSecurityConfig.java
+++ authservice/src/main/java/com/authservice/config/AppSecurityConfig.java
@@ -1,7 +1,14 @@
 package com.authservice.config;
 
+import com.authservice.service.CustomerUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,29 +18,55 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig {
+	
+	@Autowired
+	private CustomerUserDetailsService customerUserDetailsService;
+
+    String[] publicEndpoints = {
+        "/api/v1/auth/register",
+        "/api/v1/auth/login",
+        "/api/v1/auth/update-password",
+        "/v3/api-docs/**",
+        "/swagger-ui/**",
+        "/swagger-ui.html",
+        "/swagger-resources/**",
+        "/webjars/**"
+    };
 
     @Bean
     public PasswordEncoder getEncoder() {
         return new BCryptPasswordEncoder();
     }
+    
+    @Bean
+	public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+    
+    @Bean
+	public AuthenticationProvider authProvider() {
+
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+		authProvider.setUserDetailsService(customerUserDetailsService);
+		authProvider.setPasswordEncoder(getEncoder());
+
+		return authProvider;
+	}
 
     @Bean
-    public SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())  // Disable CSRF
-            .authorizeHttpRequests(auth ->
-                auth
-                    .requestMatchers(
-                        "/api/v1/auth/register",
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html",
-                        "/swagger-resources/**",
-                        "/webjars/**"
-                    ).permitAll()
-                    .anyRequest().authenticated()
-            );
+	public SecurityFilterChain securityConfig(HttpSecurity http) throws Exception{
+		
+		http.authorizeHttpRequests( req -> {
+			req.requestMatchers(publicEndpoints)
+			   .permitAll()
+			   .requestMatchers("/api/v1/admin/welcome").hasRole("ADMIN")
+			   .anyRequest()
+			   .authenticated();			
+		}).httpBasic();
+		
+		return http.csrf().disable().build();
+	}
 
-        return http.build();
-    }
+
 }
--- authservice/src/main/java/com/authservice/entity/User.java
+++ authservice/src/main/java/com/authservice/entity/User.java
@@ -27,6 +27,17 @@ public class User {
 	
 	@Column(name = "password")
 	private String password;
+	
+	@Column(name = "role")
+	private String role;
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 	public long getId() {
 		return id;
